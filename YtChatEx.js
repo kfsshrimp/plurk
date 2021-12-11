69d051424b44be1a001410c1a883f59f1ca1ab6b
kfsshrimp <65286386+kfsshrimp@users.noreply.github.com>
2021-12-12 02:49:04 +0800

    Add files via upload

--- YtChatEx.js
+++ YtChatEx.js
@@ -1,23 +1,57 @@
+var firebasejs1 = document.createElement("script");
+firebasejs1.src="https://www.gstatic.com/firebasejs/5.5.6/firebase.js";
+document.head.appendChild(firebasejs1);
+
+if( typeof(G)!=='undefined' )
+{
+    G.chat.remove();
+}
+
 var G = {
     "style":`
         #top_chat{
-            background: #000;
+            border-radius: 10px;
+            background: rgba(125,125,125,0.5);
             z-index: 9999;
             position: absolute;
-            color:#fff;
             resize: both;
             overflow: auto;
-            cursor: pointer;
+            cursor: move;
+            padding: 10px;
         }
-        #top_chat div{
-            border:1px solod #f00;
-            margin:2px;
-            padding:2px;
-            color:#000;
-            background: #fff;
+        #top_chat>div{
+            background:var(--yt-spec-general-background-a);
+            cursor: pointer;
+            border: 2px solid #000;
+            margin: 2px;
+            padding: 8px;
+            border-radius: 10px;
+        }
+        #top_chat>div a:nth-child(1){
+            display: var(--yt-live-chat-item-timestamp-display,inline);
+            margin: var(--yt-live-chat-item-timestamp-margin,0 8px 0 0);
+            color: var(--yt-live-chat-tertiary-text-color);
+            font-size: 11px;
+        }
+        #top_chat>div a:nth-child(2){
+            color: var(--yt-live-chat-moderator-color);
+            display: inline-flex;
+        }
+        #top_chat>div a:nth-child(2) div{
+            color: var(--yt-live-chat-moderator-color,#5e84f1);
+        }
+        #top_chat>div a:nth-child(3){
+            color: var(--yt-live-chat-primary-text-color,var(--yt-spec-text-primary));
+        }
+        #top_chat>div .emoji
+        {
+            width: var(--yt-live-chat-emoji-size);
+            height: var(--yt-live-chat-emoji-size);
+            margin: -1px 2px 1px;
+            vertical-align: middle;
         }
     `,
-    "selector":`[author-type="moderator"]`,
+    "selector":`[author-type="moderator"]`,//
     "style_set":()=>{
         var styleSheet = document.createElement("style");
         styleSheet.type = "text/css";
@@ -39,28 +73,77 @@ var G = {
         G.chat.style.height = `${chat_info.height}px`;
         G.chat.style.width = `${chat_info.width}px`;
 
-        document.body.prepend(G.chat);
+        window.addEventListener("resize",()=>{
+            var chat_info = G.chatframe.getBoundingClientRect();
+            G.chat.style.left = `${chat_info.left}px`;
+            G.chat.style.top = `${chat_info.top}px`;
+            G.chat.style.height = `${chat_info.height}px`;
+            G.chat.style.width = `${chat_info.width}px`;
+        });
 
+        document.body.prepend(G.chat);
 
         for(var i=0;i<G.node.length;i++)
         {
             var message = G.node[i];
-            //console.log(message[i].innerText);
 
-            //console.log(message[i].querySelector("#author-name"));
+            var html = message.innerText;
+            html = html.split("\n").map((t)=>{
+                return `<a>${t}</a>`;
+            });
+            html[2] = message.querySelector("#message").innerHTML;
 
-            console.log(message.innerText);
-                
             var div = document.createElement("div");
-            div.innerHTML = message.innerText;
-            div.style = `
-            background: #fff;
-            color:#000;
-            `;
+            div.id = message.id;
+            div.innerHTML = html.join(" ");
 
             G.chat.appendChild(div);
         }
+
+        G.chat_ref();
+
+    },
+    "chat_ref":()=>{
+
+        G.node = G.chatframe.contentDocument.querySelectorAll(G.selector);
+
+        for(var i=0;i<G.node.length;i++)
+        {
+            var message = G.node[i];
+
+            if(G.chat.querySelector(`[id="${message.id}"]`)!==null) continue;
+
+            var html = message.innerText;
+            html = html.split("\n").map((t)=>{
+                return `<a>${t}</a>`;
+            });
+            html[2] = message.querySelector("#message").innerHTML;
+
+            var div = document.createElement("div");
+            div.innerHTML = html.join(" ");
+            div.id = message.id;
+            G.chat.appendChild(div);
+        }
+
+        if(G.stop===true) return;
+
+        G.chat.scrollTo( 0,G.chat.scrollHeight );
+
+        setTimeout(()=>{G.chat_ref();},1000);
+    },
+    DB_set:()=>{
+        
+        if(typeof(firebase)==='undefined')
+        {
+            setTimeout(()=>{G.DB_set();},0);
+            return;
+        }
+        
+        G.DB = firebase;
+        G.DB.initializeApp({databaseURL:"https://kfs-plurk-default-rtdb.firebaseio.com/"});
+        G.DB = G.DB.database();
     }
+    
 
 
 };
@@ -80,5 +163,17 @@ window.addEventListener("mousedown",function(e){
 });
 
 
+
 G.style_set();
 G.chat_set();
+//G.DB_set();
+
+
+
+
+
+/*
+var js = document.createElement("script");
+js.src = "https://kfsshrimp.github.io/plurk/YtChatEx.js";
+document.head.prepend(js);
+*/
